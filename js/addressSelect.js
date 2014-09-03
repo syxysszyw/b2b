54423bfc0c9a2262d24d40b177bd95ad0df6b580
syxysszyw <545235009@qq.com>
2014-09-03 21:07:53 +0800

    b2b first commit

---
+++ js/addressSelect.js
@@ -0,0 +1,36 @@
+﻿$(function(){
+  loadProvince('440000');
+})
+
+function loadProvince(regionId){
+  $('#id_provSelect').html("");
+  $('#id_provSelect').append("<option value=''>请选择省份</option>");
+  var jsonStr = getAddress(regionId, 0);
+  for(var k in jsonStr){
+    $('#id_provSelect').append("<option value='"+k+"'>"+jsonStr[k]+"</option>");
+  }
+  if(regionId.length!=6){
+    $('#id_citySelect').html("");
+    $('#id_citySelect').append("<option value=''>请选择城市</option>");
+  }else{
+    $("#id_provSelect").val(regionId.substring(0, 2)+"0000");
+    loadCity(regionId);
+  }
+}
+
+function loadCity(regionId){
+  $("#id_citySelect").html("");
+  $("#id_citySelect").append("<option value=''>请选择城市</option>");
+  if(regionId.length == 6){
+    var jsonStr = getAddress(regionId, 1);
+    for(var k in jsonStr){
+      $("#id_citySelect").append("<option value='"+k+"'>"+jsonStr[k]+"</option>");
+    }
+    var str = regionId.substring(0, 2);
+    if(str == "11" || str == "12" || str == "31" || str == "50"){
+      $("#id_citySelect").val(regionId);
+    }else{
+      $("#id_citySelect").val(regionId.substring(0, 4)+"00");
+    }
+  }
+}
---
+++ js/distributor.js
@@ -0,0 +1,69 @@
+	$(function(){
+        $(".requiredStar").each(function(){
+            var $required = $("<em>*</em>");
+            $(this).prepend($required);
+        });
+
+        $("input[required='required']").on("focus", function(){
+            if($(this).val() == ""){
+                $(this).css("border", "1px solid #ccc");
+            }
+        });
+
+        $("input[required='required']").on("blur", function(){
+            if($(this).val() == ""){
+                $(this).css("border", "1px solid red");
+            }
+        });
+
+        function inputValidation(cla){
+            var valid = true;
+            $(cla).each(function(){
+                if($(this).val() == ""){
+                    // 如果不要弹出框就注释下面三行代码
+                    var $label = $(this).prev('label').text();
+                    $label = $label.substring(1);
+                    alert("请输入"+$label);
+                    $(this).focus();
+                    valid = false;
+                    return valid;
+                }
+            });
+            return valid;
+        }
+
+
+        $("#submitBtn").on("click", function(){
+            var valid = inputValidation("input[required='required']");
+            if(valid){
+                $("form").submit();
+            }
+        });
+        
+
+        $('#searchBtn').on('click', function(){
+            var $target = $(".b2b_right_bottom_table");
+            if($target.is(':hidden')){
+                $target.css('display', 'block');
+            }
+        });
+ 
+        $(".freeze").on("click", function(){
+            $_this = $(this);
+            $freezeVal = $_this.text();
+            $(".freeze_confirm").on("click", function(){
+                if($freezeVal == "冻结"){
+                    $_this.removeClass("btn-danger").addClass("btn-success");
+                    $(".alert_content").text("确认要恢复该用户吗？");
+                    $_this.text("恢复");
+                    $_this.parent().siblings('.tr_status').text("冻结");
+                }else{
+                    $_this.removeClass("btn-success").addClass("btn-danger");
+                    $(".alert_content").text("确认要冻结该用户吗？")
+                    $_this.text("冻结");
+                    $_this.parent().siblings('.tr_status').text("有效");
+                }
+            });
+        });
+    });
+ 
